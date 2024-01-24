6cf9cfa3da02a9171232090674c6194f7197369e
Nehahere12 <125659761+Nehahere12@users.noreply.github.com>
2024-01-24 11:17:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let progress = document.getElementById("progress");
+let song = document.getElementById("song");
+let controlicon = document.getElementById("ctrlicon");
+
+song.onloadedmetadata = function () {
+  progress.max = song.duration;
+  progress.value = song.currentTime;
+};
+function playPause() {
+  if (controlicon.classList.contains("fa-pause")) {
+    song.pause();
+    controlicon.classList.remove("fa-pause");
+    controlicon.classList.add("fa-play");
+  } else {
+    song.play();
+    controlicon.classList.add("fa-pause");
+    controlicon.classList.remove("fa-play");
+  }
+}
+if (song.play()) {
+  setInterval(() => {
+    progress.value = song.currentTime;
+  }, 500);
+}
+progress.onchange = function () {
+  song.play();
+  song.currentTime = progress.value;
+  controlicon.classList.add("fa-pause");
+  controlicon.classList.remove("fa-play");
+};
